c69ded873550c43ed0320b3fcc452acf2d1592a0
EthanF07 <ethanfang50@gmail.com>
2024-01-18 19:30:41 -1000

    Apriltags code

---
+++ src/main/java/frc/robot/commands/IdentifyAprilTagCommand.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AprilTagsSubsystem;
+import frc.robot.RobotContainer;
+
+/*
+ * Filler Command for the detection of the apriltag
+ */
+public class IdentifyAprilTagCommand extends Command{
+    // Declaring Subsystems
+
+    private AprilTagsSubsystem.Pipeline pipeline;
+
+    public IdentifyAprilTagCommand() {
+
+    }
+
+}
